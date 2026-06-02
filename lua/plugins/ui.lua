a3f72f28095744621bd92ff62142db23427f47a9
Anton Kiiski <antonoliverkiiski@gmail.com>
2026-06-02 14:26:41 +0300

    swap coloscheme to tokyonight

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,18 @@
 return {
   {
-    'loctvl842/monokai-pro.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme('monokai-pro')
+      vim.cmd.colorscheme('tokyonight')
       vim.cmd.hi('Comment gui=none')
     end,
   },
+  -- {
+  --   'loctvl842/monokai-pro.nvim',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme('monokai-pro')
+  --     vim.cmd.hi('Comment gui=none')
+  --   end,
+  -- },
 }
